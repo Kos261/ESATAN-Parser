25db16cb0986cafe009161d79f486aff6fa16136
Kos261 <konstanty.klosiewicz@gmail.com>
2023-11-19 23:55:58 +0100

    Updated catalogs and clearer hierarchy

---
+++ Esatan_case_folder/01_GMM/Test_subj_1.bat
@@ -0,0 +1,7 @@
+
+set	PFAD_GMM=C:\Users\koste\OneDrive\Pulpit\AT_ParserFiles\01_GMM\
+
+rem call submodels
+call C:\ESATAN-TMS\2018sp1\Radiative\bin\esrdg.bat < %PFAD_GMM%02_SUBMODELS\1_TCS_FPM_TVAC.erg >      %PFAD% 1_TCS_FPM_TVAC.out
+
+pause
